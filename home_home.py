b10252d653e4202a09b58141af50c7b3f50a9f96
sss3287 <sss3287@rit.edu>
2021-11-29 10:59:12 +0400

    Class home category search for item

--- home_home.py
+++ home_home.py
@@ -6,4 +6,31 @@ class Home_Category:
         self.__code = code
         self.__price = price
 
+    def garden_search(self, name, code):
+        GARDEN={"p": ("3 pack garden flower", 5.0), "l": ("Hanging light wire", 10.0), "b": ("Garden bench", 35.0)}
+
+        for i in GARDEN.keys():
+            if i==code or GARDEN[i][0]==name:
+                self.__name=name
+                self.__code=code
+                self.__price=GARDEN[i][1]
+
+    def indoor_search(self, name, code):
+        INDOOR={"t": ("Small table lamp", 5.0), "f1": ("City picture frame", 7.0), "r": ("4x5 entry rug", 35.0),"f2": ("flower vase", 14.0)}
+
+        for i in INDOOR.keys():
+            if i==code or INDOOR[i][0]==name:
+                self.__name=name
+                self.__code=code
+                self.__price=INDOOR[i][1]
+
+    def bathroom_search(self, name, code):
+        BATHROOM={"s": ("Soap holder", 5.0), "c": ("Shower Curtain", 5.0), "m": ("anti-skid doormat", 30.0)}
+
+        for i in BATHROOM.keys():
+            if i==code or BATHROOM[i][0]==name:
+                self.__name=name
+                self.__code=code
+                self.__price=BATHROOM[i][1]
+
 
