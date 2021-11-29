0f5af7ad4769a557244d15275cc57700838f0dd0
sss3287 <sss3287@rit.edu>
2021-11-29 23:11:44 +0400

    many modifications

--- test.py
+++ test.py
@@ -1,2 +1,26 @@
-import home_home
+import home_home as hh
+
+
+
+def test_indoor_search():
+    '''This function tests if the function searches indoor items'''
+    x=hh.Home_Category("", "", 0)
+    assert(x.search_indoor("f1") == True)
+
+def test_bathroom_search():
+    '''This function tests if the function searches bathroom items'''
+    x=hh.Home_Category("", "", 0)
+    assert(x.search_bathroom("b") == True)
+
+def test_garden_search():
+    '''This function tests if the function searches garden items'''
+    x=hh.Home_Category("", "", 0)
+    assert(x.search_garden("p") == True)
+
+
+def test_garden_order():
+    '''This function tests if the function searches garden items'''
+    x=hh.garden_order([])
+    assert(x == True)
+
 
