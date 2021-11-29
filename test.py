84af68abc5d2f9dacbadd12e363763274d9308bd
sss3287 <sss3287@rit.edu>
2021-11-29 23:25:47 +0400

    many modifications

--- test.py
+++ test.py
@@ -1,34 +1,34 @@
-import home_home as hh
+import home_home
 
 def test_indoor_search():
     '''This function tests if the function searches indoor items'''
-    x=hh.Home_Category("", "", 0)
+    x=home_home.Home_Category("", "", 0)
     assert(x.search_indoor("f1") == True)
 
 def test_bathroom_search():
     '''This function tests if the function searches bathroom items'''
-    x=hh.Home_Category("", "", 0)
-    assert(x.search_bathroom("b") == True)
+    x=home_home.Home_Category("", "", 0)
+    assert(x.search_bathroom("c") == True)
 
 def test_garden_search():
     '''This function tests if the function searches garden items'''
-    x=hh.Home_Category("", "", 0)
+    x=home_home.Home_Category("", "", 0)
     assert(x.search_garden("p") == True)
 
 
 def test_garden_order():
     '''This function tests if the function searches garden items'''
-    x=hh.garden_order([])
-    assert(x == True)
+    x=home_home.garden_order(0, [1])
+    assert(x == None)
 
 def test_indoor_order():
     '''This function tests if the function searches garden items'''
-    x=hh.garden_order([])
+    x=home_home.garden_order([])
     assert(x == True)
 
 def test_bathroom_order():
     '''This function tests if the function searches garden items'''
-    x=hh.bathroom_order([])
+    x=home_home.bathroom_order([])
     assert(x == True)
 
 
